9a58d88de35fbfa09533db260a642fa5d936584b
lomaTT <alexeyttdeveloper@gmail.com>
2023-07-28 14:45:29 +0200

    initial commit, added basic styles and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BaseComponent} from "./components/base/base.component";
+import {ProductsComponent} from "./components/products/products.component";
+import {ProductDetailsComponent} from "./components/product-details/product-details.component";
+import {BasketComponent} from "./components/basket/basket.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BaseComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'product/:id', component: ProductDetailsComponent},
+  {path: 'basket', component: BasketComponent},
+  {path: "**", redirectTo: "", component: BaseComponent} // if path not found - redirect to base component
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/ui/header/header.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { BaseComponent } from './components/base/base.component';
+import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from '@angular/material/card';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    BasketComponent,
+    ProductDetailsComponent,
+    BaseComponent,
+    DialogBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
